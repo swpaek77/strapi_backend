02086a23cb313bfa534c656075861f716457ad46
Seungwoo Paek <swpaek77@gmail.com>
2021-11-29 00:44:36 +0900

    walletEnroll 추가

--- api/message/controllers/message.js
+++ api/message/controllers/message.js
@@ -12,6 +12,7 @@ module.exports = {
     // console.log(ctx.query);
 
     try {
+      let kakaouid = 0;
       const body = ctx.request.body;
 
       if (!body.sender || !body.imageProfileBase64 || !body.room || !body.message) {
@@ -32,10 +33,6 @@ module.exports = {
           imageProfileBase64: body.imageProfileBase64,
         }
       );
-
-      ctx.send(step1[0]);
-
-      let kakaouid = 0;
 
       // 유저 정보가 없는 경우
       if (step1[0].length === 0) {
@@ -79,6 +76,96 @@ module.exports = {
           message: body.message,
         }
       );
+
+      if (step3[0].affectedRows === 1) {
+        ctx.send({ result: 'SUCCESS', message: 'Successfully Inserted' });
+      } else {
+        ctx.send({ result: 'ERROR', message: 'DB Error' });
+      }
+    } catch (err) {
+      ctx.send({ result: 'ERROR', message: 'Critical Error ![' + err.message + ']' });
+    }
+  },
+  walletEnroll: async (ctx) => {
+    try {
+      const body = ctx.request.body;
+
+      if (!body.sender || !body.imageProfileBase64 || !body.walletKey) {
+        ctx.send({ result: 'ERROR', message: 'Validation Error' });
+        return;
+      }
+
+      // 1. 해당 walletKey 초기화
+      const step1 = await strapi.connections.default.raw(
+        `
+        UPDATE 
+          kakaouids
+        SET 
+          walletKey = NULL
+        WHERE 
+          walletKey = :walletKey
+        `,
+        {
+          walletKey: body.walletKey,
+        }
+      );
+
+      // 2. 유저정보 확인
+      const step2 = await strapi.connections.default.raw(
+        `
+        SELECT *
+        FROM kakaouids
+        WHERE
+          sender = :sender AND
+          imageProfileBase64 = :imageProfileBase64
+        `,
+        {
+          sender: body.sender,
+          imageProfileBase64: body.imageProfileBase64,
+        }
+      );
+
+      let step3 = [];
+      // 3. 유저 정보가 없으면 Insert, 있으면 Update
+      if (step2[0].length === 0) {
+        step3 = await strapi.connections.default.raw(
+          `
+          INSERT INTO kakaouids
+          (
+            sender,
+            imageProfileBase64,
+            walletKey
+          )
+          VALUES (
+            :sender,
+            :imageProfileBase64,
+            :walletKey
+          );
+          `,
+          {
+            sender: body.sender,
+            imageProfileBase64: body.imageProfileBase64,
+            walletKey: body.walletKey,
+          }
+        );
+      } else {
+        step3 = await strapi.connections.default.raw(
+          `
+          UPDATE 
+            kakaouids
+          SET 
+            walletKey = :walletKey
+          WHERE 
+            sender = :sender AND
+            imageProfileBase64 = :imageProfileBase64;
+          `,
+          {
+            sender: body.sender,
+            imageProfileBase64: body.imageProfileBase64,
+            walletKey: body.walletKey,
+          }
+        );
+      }
 
       if (step3[0].affectedRows === 1) {
         ctx.send({ result: 'SUCCESS', message: 'Successfully Inserted' });
